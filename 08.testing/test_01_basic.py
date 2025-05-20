cb8c1b1badbd05f50c07f37f18176057d1bea831
Murat Ozcan <murat@Murats-MacBook-Pro.local>
2025-05-20 06:09:04 -0500

    test: basics

---
+++ 08.testing/test_01_basic.py
@@ -0,0 +1,15 @@
+import pytest 
+
+def multiple_of_two(num):
+	if num == 0:
+		raise(ValueError)
+	return num % 2 == 0
+
+def test_numbers():
+	assert multiple_of_two(2) == True
+	assert multiple_of_two(3) == False
+
+def test_zero():
+	# need to add a context for an exception test
+	with pytest.raises(ValueError):
+		multiple_of_two(0)
