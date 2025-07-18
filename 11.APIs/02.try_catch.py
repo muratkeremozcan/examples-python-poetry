164faf1f0fe4fefa9da5ab2c20d4bff1c86b6da9
Murat Ozcan <murat@mac.lan>
2025-07-18 09:27:48 -0500

    try catch with request

--- 11.APIs/02.try_catch.py
+++ 11.APIs/02.try_catch.py
@@ -13,28 +13,3 @@ except ConnectionError as conn_err:
 except HTTPError as http_err:
     print(f'HTTP error occurred: {http_err}')
 
-
-####
-# 
-while True:
-    params = {'page': page_number, 'per_page': 500}
-    response = requests.get('http://localhost:3000/tracks', params=params, headers=headers)
-    response.raise_for_status()
-    response_data = response.json()
-    
-    print(f'Fetching tracks page {page_number}')
-
-    if len(response_data['results']) == 0:
-        break
-
-    for track in response_data['results']:
-        if(track['Length'] > longestTrackLength):
-            longestTrackLength = track['Length']
-            longestTrackTitle = track['Name']
-
-    page_number = page_number + 1
-    
-    # Add your fix here
-    time.sleep(3)
-
-print('The longest track in my music library is: ' + longestTrackTitle)		
