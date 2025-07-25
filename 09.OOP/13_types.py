7cda26d3488af5bb741ad234a5688851153c41d7
Murat Ozcan <murat@mac.lan>
2025-07-25 11:14:55 -0500

    more types

--- 09.OOP/13_types.py
+++ 09.OOP/13_types.py
@@ -12,3 +12,26 @@ agents: List[str] = [
 ]
 
 print(agents)
+
+#############
+
+class Agent:
+  def __init__(self, codename: str, missions: int):
+    self.codename: str = codename
+    self.missions: int = missions
+
+  # Create the add_mission() method, add type hinting
+  def add_mission(self, location: str) -> None:
+    self.missions += 1
+    print(f"{self.codename} completed a mission in " + \
+          f"{location}. This was mission #{self.missions}")
+
+# Create an Agent object, add type hints
+chuck: Agent = Agent("Charles Carmichael", 37)
+
+# Create a list of locations, add a mission for each
+locations: List[str] = ["Burbank", "Paris", "Prague"]
+for location in locations:
+  chuck.add_mission(location)
+
+print(chuck.missions)
