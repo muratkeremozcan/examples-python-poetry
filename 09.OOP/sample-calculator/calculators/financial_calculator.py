484978a3a9d40a8231343aa41ccb084717889d97
Murat Ozcan <murat@mac.lan>
2025-08-05 07:54:42 -0500

    feat: custom class

--- 09.OOP/sample-calculator/calculators/financial_calculator.py
+++ 09.OOP/sample-calculator/calculators/financial_calculator.py
@@ -1,8 +1,8 @@
 from basic_calculator import BasicCalculator
 
 class FinancialCalculator(BasicCalculator):
-	def monthly_interest(self, annual_interest_rate):
+	def monthly_interest(self, annual_interest_rate: float) -> float:
 		return self.divide(annual_interest_rate, 12)
 
-	def month_from_years(self, years):
+	def month_from_years(self, years: int) -> int:
 		return self.multiply(years, 12)
--- 09.OOP/sample-calculator/calculators/mortgage_calculator.py
+++ 09.OOP/sample-calculator/calculators/mortgage_calculator.py
@@ -20,3 +20,21 @@ class MortgageCalculator(FinancialCalculator):
 		self.loan_amount: float = loan_amount
 		self.monthly_interest_rate: float = self.monthly_interest(annual_interest_rate)
 		self.months: int = self.month_from_years(years)
+		self.monthly_payment: float = self.calculate_monthly_payment()
+
+	def calculate_monthly_payment(self):
+		numerator: float = self.monthly_interest_rate * (1 + self.monthly_interest_rate) ** self.months
+		denominator: float = (1 + self.monthly_interest_rate) ** self.months - 1
+		multiplier: float = self.divide(numerator, denominator)
+		monthly_payment: float = round(self.multiply(self.loan_amount, multiplier), 2)	
+		return monthly_payment
+
+# use help(...object...) to get documentation
+# use dir(...object...) to get list of attributes and methods
+# print(help(FinancialCalculator))
+# print(dir(FinancialCalculator))
+
+
+  
+mortgage_calculator = MortgageCalculator(300000, 0.059, 10)
+print(f"Your monthly mortgage payment is ${round(mortgage_calculator.monthly_payment, 2)}.")
